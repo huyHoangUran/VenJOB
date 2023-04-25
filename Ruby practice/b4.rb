d60ffbcba0f1beed2704eae4a462c922e1b9003a
hoangnh <hoangnh@zigexn.vn>
2023-04-25 17:19:47 +0700

    b4 toNum

--- Ruby practice/b4.rb
+++ Ruby practice/b4.rb
@@ -36,6 +36,52 @@ def to_kansuji(number=0)
   result
 end
 
+def to_number(kansuji)
+  number = 0
+  thousands = 0
+  hundreds = 0
+  tens = 0
+  ones = 0
+
+  kansuji.chars.each do |char|
+    if char == 'マ'
+      number = -number
+    elsif char == '千'
+      number += thousands * 1000
+      thousands = 0
+    elsif char == '百'
+      number += hundreds * 100
+      hundreds = 0
+    elsif char == '十'
+      if hundreds == 0
+        tens = 1
+      else
+        number += (tens + 1) * 10
+        tens = 0
+      end
+    else
+      value = KANJI_MAP_REVERSE[char]
+      if tens > 0
+        number += (tens + 1) * value
+        tens = 0
+      elsif hundreds > 0
+        number += hundreds * 100 + value
+        hundreds = 0
+      elsif thousands > 0
+        number += thousands * 1000 + value
+        thousands = 0
+      else
+        ones = value
+      end
+    end
+  end
+
+  number += thousands * 1000 + hundreds * 100 + (tens + 1) * 10 + ones
+  number
+end
+
 puts "nhap vao so muon chuyen"
 numIn = gets.chomp().to_i
 puts to_kansuji(numIn)
+
+
