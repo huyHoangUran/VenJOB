a11256d96083a6f1289069df51d9e3fe7b693add
HuyHoang <huyhoangnguyen.work@gmail.com>
2023-06-19 09:16:53 +0700

    delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema[7.0].define(version: 2023_06_18_174734) do
   create_table "cities", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
