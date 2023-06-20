629c4fabed031bc761acdf8e872d8f54157b4b1d
HuyHoang <huyhoangnguyen.work@gmail.com>
2023-06-20 09:58:20 +0700

    update

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -7,7 +7,6 @@
     def create
       self.resource = resource_class.send_reset_password_instructions(resource_params)
       yield resource if block_given?
-  
       if successfully_sent?(resource)
         respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
       else
@@ -22,15 +21,28 @@
       reset_password_token = params[:reset_password_token]
       @user = resource_class.with_reset_password_token(params[:reset_password_token])
       @user_email = @user&.email if @user
+      resource.reset_password_token = params[:reset_password_token]
     end
 
-    def update
+     def update
       self.resource = resource_class.reset_password_by_token(resource_params)
+      yield resource if block_given?
+
       if resource.errors.empty?
-        sign_in(resource_name, resource)
-        redirect_to root_path
+        resource.unlock_access! if unlockable?(resource)
+        if resource_class.sign_in_after_reset_password
+          flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+          set_flash_message!(:notice, flash_message)
+          resource.after_database_authentication
+          sign_in(resource_name, resource)
+        else
+          set_flash_message!(:notice, :updated_not_active)
+        end
+        binding.pry
+        respond_with resource, location: after_resetting_reset_password_path_for(resource)
       else
-        render :edit
+        set_minimum_password_length
+        respond_with resource
       end
     end
   end
