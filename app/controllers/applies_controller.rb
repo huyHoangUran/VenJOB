4787d1d0ac96c5d2ff661a715ca989243e81bc18
HuyHoang <huyhoangnguyen.work@gmail.com>
2023-06-23 09:30:15 +0700

    add view applied for user

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -1,6 +1,9 @@
 class AppliesController < ApplicationController
   before_action :authenticate_user!
-
+  def index
+    @applied_jobs = current_user.applies
+  end
+  
   def new_apply
     @apply = Apply.new
   end
